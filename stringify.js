e26fd8be911f4bfaf89b9eb474c29de7719d8a7a
aksanoble <aksanoble@gmail.com>
2015-04-23 02:26:30 +0530

    minor

--- stringify.js
+++ stringify.js
@@ -1,10 +1,23 @@
+//Simple program to convert stringify not dealing with Array and Null value types
+
 // Test Object
-var myObj = {a: "foo", b: 2, c: {a:"a", b:"b"}, d: true, e:null};
+var myObj = {a: "foo", b: 2, c: {a:"a", b:"b"}, d: true};
+
+// To find the total number of keys present. This helps us stringify at the end
+function objToArray (obj) {
+  var arr = [];
+  for (var key in obj) {
+    arr.push(obj[key]);
+  }
+  return arr;
+}
 
 // Add quotes to string types simply return the rest
 function addQuotes(element) {
     if(typeof element == "string") return '"' + element + '"';
-    else return element;
+    else if(typeof element == "number") return element;
+    else if (typeof element == "boolean") return element;
+    else if(typeof element == "object") return element;
   }
 
 // Main Stringify function
@@ -21,9 +34,10 @@ function myStr(obj) {
       }
 
 // Do not add Comma at the End
-      if (i != ((Object.keys(myObj).length)))
+      if (i != (Object.keys(obj).length))
         string += ",";
- }
+
+  }
 
   return "{"+string+"}";
 }
